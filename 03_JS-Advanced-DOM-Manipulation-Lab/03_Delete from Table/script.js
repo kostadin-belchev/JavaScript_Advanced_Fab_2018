5c2620c1e6d6cf957d85c52f06a6d8426c812bd6
Kostadin Belchev <kostadin.belchev@gmail.com>
2018-02-17 12:16:48 +0200

    did until task 4

--- 03_JS-Advanced-DOM-Manipulation-Lab/03_Delete from Table/script.js
+++ 03_JS-Advanced-DOM-Manipulation-Lab/03_Delete from Table/script.js
@@ -1,3 +1,15 @@
 function deleteByEmail() {
-    
+    let tableRows = document.getElementsByTagName('tr');
+    let emailToBeChecked = document.getElementsByName('email')[0].value;
+    for (let row = 1; row < tableRows.length; row++) {
+        let currEmail = tableRows[row].children[1].textContent;
+        console.log(`${currEmail} -> ${emailToBeChecked}`);
+        if (currEmail === emailToBeChecked) { // delete the email
+            tableRows[row].parentNode.removeChild(tableRows[row]);
+            document.getElementById('result').textContent = 'Deleted.';
+            return;
+        }
+    }
+    // otherwise print error that email is not found
+    document.getElementById('result').textContent = "Not found.";
 }
