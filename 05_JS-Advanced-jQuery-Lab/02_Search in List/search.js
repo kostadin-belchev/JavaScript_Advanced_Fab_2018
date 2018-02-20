d97ee797f1deed168fae6b2e2f84f2fa130a4aa3
Kostadin Belchev <kostadin.belchev@gmail.com>
2018-02-20 10:24:50 +0200

    finished task 3 100% in Judge, we can just make it prettier now

--- 05_JS-Advanced-jQuery-Lab/02_Search in List/search.js
+++ 05_JS-Advanced-jQuery-Lab/02_Search in List/search.js
@@ -1,11 +1,17 @@
 //Created by *** Kostadin Belchev ***
 function search() {
     let searchPattern = $('#searchText');
-    $('#items>li').each((index, element) => {
+    let matches = 0;
+    $('#towns li').each((index, element) => {
         if (element.textContent.includes(searchPattern.val())) {
-
+            $(element).css('font-weight', 'bold');
+            matches++;
+        } else {
+            $(element).css('font-weight', '');
         }
     });
+    $('#result').text(`${matches} matches found.`);
+    // let searchPattern = $('#searchText');
     // $('#towns li').css('font-weight', '');
     // let matchedItems = $(`#towns li:contains(${searchPattern.val()})`).css('font-weight', 'bold');
     // $('#result').text(`${matchedItems.length} matches found.`);
---
+++ 05_JS-Advanced-jQuery-Lab/03_Countries Table/initialize-table.js
@@ -0,0 +1,68 @@
+function initializeTable() {
+
+    $('#createLink').click(createCountry);
+    addCountry('Bulgaria', 'Sofia');
+    addCountry('Germany', 'Berlin');
+    addCountry('Russia', 'Moscow');
+    fixLinks();
+
+    //fixLinks function
+    function fixLinks () {
+        // Show all links in the table
+        $('tr').find('a').css('display', 'inline');
+
+        // Hide [Up] link in first table data row
+        $(`tr:eq(2)`).find("a:contains('Up')").css('display', 'none');
+
+        // Hide the [Down] link in the last table row
+        $(`tr:last-child`).find("a:contains('Down')").css('display', 'none');
+    }
+
+
+    //created country from the [Create] button
+    function createCountry () {
+        let countryName = $('#newCountryText');
+        let capitalName = $('#newCapitalText');
+        addCountry(countryName.val(), capitalName.val());
+        countryName.val('');
+        capitalName.val('');
+        fixLinks();
+    }
+    // adds country to DOM tree
+    function addCountry (countryName, capitalName) {
+        let row = $('<tr>')
+        .append($('<td>').text(countryName))
+        .append($('<td>').text(capitalName))
+        .append($('<td>')
+            .append($('<a href="#">').text('[Up]').click(moveUp))
+            .append(' ')
+            .append($('<a href="#">').text('[Down]').click(moveDown))
+            .append(' ')
+            .append($('<a href="#">').text('[Delete]').click(deleteItem))
+        );
+        row.appendTo('#countriesTable');
+    }
+
+    //moveUp function
+    function moveUp () {
+        //alert('Moved Up!') // checking if linkage works
+        let row = $(this).parent().parent();
+        row.insertBefore(row.prev());
+        fixLinks();
+    }
+
+    //moveDown function
+    function moveDown(params) {
+        //alert('Moved Down!')
+        let row = $(this).parent().parent();
+        row.insertAfter(row.next());
+        fixLinks();
+    }
+
+    //deleteItem function
+    function deleteItem(params) {
+        //alert('Deleted!')
+        $(this).parent().parent().remove();
+        fixLinks();
+    }
+}
