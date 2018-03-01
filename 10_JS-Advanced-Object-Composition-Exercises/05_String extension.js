e21584e474e5dc738d53821e29ff54f350de644a
Kostadin Belchev <kostadin.belchev@gmail.com>
2018-03-01 17:19:10 +0200

    still at task 5 of exercises of Object Composition

--- 10_JS-Advanced-Object-Composition-Exercises/05_String extension.js
+++ 10_JS-Advanced-Object-Composition-Exercises/05_String extension.js
@@ -34,47 +34,38 @@
             string = '.'.repeat(n);
         } else if (string.indexOf(' ') < 0) { // If no space occurs anywhere in the string, return n – 3 characters and an ellipsis
             string = string.slice(0, n - 3) + elipsis;
-
         } else if (string.length >= n) { // If a string is less than n characters long, return the same string, meaning it does not enter in this if statement
         // If it is longer, meaning it entered in this if statement, 
         // split the string where a space occurs and append an ellipsis to it so that the total length is less than or equal to n.
             let stringTokens = string.split(' ');
             stringTokens;
             let counter = 0;
-            while (string.length > n + elipsis.length) {
+            while (string.length + elipsis.length > n) {
                 string = stringTokens[counter] + " " + stringTokens[counter + 1];
+                string;
                 counter++;
             }
             string = string + elipsis;
+            string;
         }
         return string;
     }
 })();
 
 
-let str = 'my string';
-str = str.ensureStart('my'); // 'my' already present
-str = str.ensureStart('hello ');
-str = str.ensureEnd('ing');
-//str = str.truncate(16);
+let str = 'my string'
+str = str.ensureStart('my')
 str;
-//str = str.truncate(14);
+str = str.ensureStart('hello ')
 str;
-test = 'testing'.truncate(6);
-test;
-test1 = str.truncate(8);
-test1;
-test1 = test1.truncate(4);
-test1;
-
-
-test2 = 'hello '.slice(0, 'hello '.length - 1);
-test2;
-
-// str = str.truncate(16) 
+// str = str.truncate(16)
+// str;
 // str = str.truncate(14)
+// str;
 // str = str.truncate(8)
-// str = str.truncate(4)
+// str;
+//str = str.truncate(4)
+str;
 // str = str.truncate(2)
 // str = String.format('The {0} {1} fox',  'quick', 'brown');
 // str = String.format('jumps {0} {1}',  'dog');
