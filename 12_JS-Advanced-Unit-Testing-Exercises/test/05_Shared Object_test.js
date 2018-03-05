3f04ee71df51f5527c4dcd42809caf8693ffacd4
Kostadin Belchev <kostadin.belchev@gmail.com>
2018-03-05 19:00:50 +0200

    finished exercises for unit testing, starting classes lab

--- 12_JS-Advanced-Unit-Testing-Exercises/test/05_Shared Object_test.js
+++ 12_JS-Advanced-Unit-Testing-Exercises/test/05_Shared Object_test.js
@@ -1,5 +1,4 @@
 let expect = require('chai').expect;
-//let jsdom = require('jsdom');
 let jsdom = require('jsdom-global')();
 let $ = require('jquery');
 const sharedObject = require('../05_Shared Object');
@@ -66,6 +65,76 @@ describe('sharedObject Unit testing', function () {
             sharedObject.changeIncome(2000);
             let incomeTextboxValue = $('#income').val();
             expect(incomeTextboxValue).to.equal('2000');
+        });
+
+        // edge case with 0
+        it('income with a 0, should return with no changes', function () {
+            sharedObject.changeIncome(1000);
+            sharedObject.changeIncome(0);
+            expect(sharedObject.income).to.equal(1000);
+        });
+    });
+
+    describe('updateName() tests', function () {
+        // In case the name textbox's value is an empty string - no changes should be made,
+        // alternatively the name property of the sharedObject should be set to the value of the name textbox. 
+        it("with name textbox's value is an empty string, should return with no changes", function () {
+            sharedObject.changeName('Stoyan');
+            let nameField = $('#name').val('');
+            sharedObject.updateName();
+            expect(sharedObject.name).to.equal('Stoyan');
+        });
+
+        it("name textbox's value with a non-empty string, should return the non-empry string", function () {
+            let nameField = $('#name').val('Kosta');
+            sharedObject.updateName();
+            expect(sharedObject.name).to.equal('Kosta');
+        });
+        
+    });
+
+    describe('updateIncome() tests', function () {
+        // In case the income textbox's value cannot be parsed to a positive integer - no changes should be made,
+        // alternatively the income property of the sharedObject should be set to the value of the income textbox.
+        it("with income textbox's value that cannot be parsed to a positive integer(e.g. string), should return with no changes", function () {
+            sharedObject.changeIncome(1000);
+            let incomeField = $('#income');
+            incomeField.val('pesho');
+            sharedObject.updateIncome();
+            expect(sharedObject.income).to.equal(1000);
+        });
+
+        it("with income textbox's value that cannot be parsed to a positive integer(e.g. floating point), should return with no changes", function () {
+            sharedObject.changeIncome(1000);
+            let incomeField = $('#income');
+            incomeField.val('3.14');
+            sharedObject.updateIncome();
+            expect(sharedObject.income).to.equal(1000);
+        });
+
+        it("income textbox's value with a valid interger, should set value of income property of sharedOnbect to that value", function () {
+            sharedObject.changeIncome(3000);
+            let incomeField = $('#income');
+            incomeField.val(500);
+            sharedObject.updateIncome();
+            expect(sharedObject.income).to.equal(500);
+        });
+
+        it("income textbox's value with a negative, should return with no changes", function () {
+            sharedObject.changeIncome(3000);
+            let incomeField = $('#income');
+            incomeField.val(-100);
+            sharedObject.updateIncome();
+            expect(sharedObject.income).to.equal(3000);
+        });
+        
+        // edge case with 0
+        it("income textbox's value with 0, should return with no changes", function () {
+            sharedObject.changeIncome(3000);
+            let incomeField = $('#income');
+            incomeField.val(0);
+            sharedObject.updateIncome();
+            expect(sharedObject.income).to.equal(3000);
         });
     });
 });
